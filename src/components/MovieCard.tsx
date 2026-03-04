cee283975468cbff7c7a75acec2619abf8e852fb
Le Nguyen Minh <lenminh002@gmail.com>
2026-03-04 13:40:38 -0800

    add files

---
+++ src/components/MovieCard.tsx
@@ -0,0 +1,15 @@
+import "../css/MovieCard.css"
+
+function MovieCard() {
+    return (
+        <div className="movie-card">
+            <img src="" alt="" />
+            <div className="movie-info">
+                <h3></h3>
+                <p></p>
+            </div>
+        </div>
+    )
+}
+
+export default MovieCard
